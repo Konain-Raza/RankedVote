cdc5c20dc44d4cc4630fa2b6d01ce8159b5184b9
Konain <konainraza105@gmail.com>
2024-05-01 20:46:03 +0500

    static version

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#element', {
+  strings: ['running contests', 'selecting leaders', 'conducting surveys', 'taking polls', 'educating voters', 'making decisions'],
+  typeSpeed: 50,
+  backSpeed: 0,
+  fadeOut: true,
+  loop: true
+});
+
